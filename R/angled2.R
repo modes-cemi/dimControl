4d5b498b95987bfd72a11660b7a3fc1645fbf7f2
GroupMODES <nataly.romaris@udc.es>
2025-11-06 13:02:10 +0100

    Actualización de función angled2()

--- R/angled2.R
+++ R/angled2.R
@@ -2,14 +2,14 @@
 #'
 #' Calcula el ángulo direccional entre un conjunto de vectores normalizados y una
 #' dirección de referencia en una dimensión específica. Permite obtener el resultado
-#' en grados o radianes y considerar la dirección negativa si es necesario.
+#' en grados o radianes, y considerar la dirección opuesta si se desea.
 #'
 #' @param v Matriz numérica de dimensión \eqn{d \times n}. Cada columna representa un vector normalizado.
-#' @param dim Índice de la dimensión de referencia, con \eqn{1 \leq \text{dim} \leq d}.
-#' @param negdir Valor lógico. Si es `TRUE`, considera la dirección negativa en el cálculo del ángulo.
-#' @param deg Valor lógico. Si es `TRUE`, devuelve los ángulos en grados; si es `FALSE`, en radianes.
+#' @param dim Entero que indica la dimensión de referencia, con \eqn{1 \leq \text{dim} \leq d}.
+#' @param negdir Lógico. Si es `TRUE`, considera la dirección negativa (ángulo complementario).
+#' @param deg Lógico. Si es `TRUE`, devuelve los ángulos en grados; si es `FALSE`, en radianes.
 #'
-#' @returns Vector numérico con los ángulos correspondientes a cada vector de entrada.
+#' @returns Un vector numérico con los ángulos correspondientes a cada vector de entrada.
 #'
 #' @examples
 #' # Tres vectores normalizados en las direcciones X, Y y Z
@@ -28,7 +28,7 @@
 #'
 #' @export
 angled2 <- function(v, dim, negdir = FALSE, deg = TRUE) {
-  # Función para convertir radianes a grados
+  # Conversión de radianes a grados
   rad2deg <- function(x) (x / pi) * 180
 
   # Cálculo del ángulo direccional
