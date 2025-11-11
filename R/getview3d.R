de9accfb35aef6602f4132a0d0f9ad51ad2ef859
GroupMODES <nataly.romaris@udc.es>
2025-11-11 11:07:27 +0100

    Se agrega función setview3d()

---
+++ R/getview3d.R
@@ -0,0 +1,10 @@
+#' Obtener la vista 3D actual
+#'
+#' Recupera los parámetros actuales de la vista 3D del dispositivo activo de rgl.
+#'
+#' @returns Lista con `zoom`, `userMatrix` y `userProjection`.
+#'
+#' @export
+getview3d <- function() {
+  par3d()[c("zoom", "userMatrix", "userProjection")]
+}
